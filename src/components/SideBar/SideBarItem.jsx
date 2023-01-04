9a24207962157b271e5c88e5568ecf98269fe5cc
ambition_echo <ambition_echo@outlook.com>
2023-01-04 11:05:12 +0800

    fix: 解决回退导航栏没有变化的问题

--- src/components/SideBar/SideBarItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { Button } from '@mui/material'
-
-export default function SideBarItem(props) {
-    return (
-        <Button sx={{ marginBottom: "8px" }} fullWidth component={NavLink} {...props} />
-    )
-}
--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,27 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Box, List } from '@mui/material'
 import IosShareOutlinedIcon from '@mui/icons-material/IosShareOutlined';
 import MarkEmailUnreadOutlinedIcon from '@mui/icons-material/MarkEmailUnreadOutlined';
 import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
-import SideBarItem from './SideBarItem'
+import { NavLink, useLocation } from 'react-router-dom';
+import { Button } from '@mui/material'
+
+
+function SideBarItem(props) {
+    return <Button sx={{ marginBottom: "8px" }} fullWidth component={NavLink} {...props} />
+}
 
 export default function SideBar() {
-    const [key, setKey] = useState('committer')
+    const location = useLocation()
 
-    function setStyle(name) {
-        return key === name ? "contained" : "outlined"
+    function setStyle(pathname) {
+        return location.pathname === pathname ? "contained" : "outlined"
     }
 
     return (
-        <Box sx={{ padding: "0 8px", overflow: 'auto' }} maxHeight='90vh'>
-            <List>
-                <SideBarItem to="/committer" variant={setStyle('committer')} onClick={() => setKey("committer")} startIcon={<IosShareOutlinedIcon />}>创建提单</SideBarItem>
+        <Box sx={{ padding: "8px", overflow: 'auto' }} maxHeight='90vh'>
+            <SideBarItem to="/committer" variant={setStyle("/committer")} startIcon={<IosShareOutlinedIcon />}>创建提单</SideBarItem>
 
-                <SideBarItem to="/assigned" variant={setStyle('assigned')} onClick={() => setKey("assigned")} startIcon={<MarkEmailUnreadOutlinedIcon />}>提到我的</SideBarItem>
+            <SideBarItem to="/assigned" variant={setStyle("/assigned")} startIcon={<MarkEmailUnreadOutlinedIcon />}>提到我的</SideBarItem>
 
-                <SideBarItem to="/my-commits" variant={setStyle('my-commits')} onClick={() => setKey("my-commits")} startIcon={<Inventory2OutlinedIcon />}>我的提交</SideBarItem>
-            </List>
+            <SideBarItem to="/my-commits" variant={setStyle("/my-commits")} startIcon={<Inventory2OutlinedIcon />}>我的提交</SideBarItem>
         </Box >
-
     )
 }
